e2685c5dbdc3f25a89a377af838b9d2c7d4b08f5
Ankush Roy <ankush2@cs.ualberta.ca>
2013-10-21 18:28:26 -0600

    Cleaned

--- Manifold.py
+++ Manifold.py
@@ -57,16 +57,48 @@ end
 
 """
 # Author: ankush2@ualberta.ca
-import numpy
-from numpy import matrix as MA
-def WineKliyeKuchBhi(Xtrain,Ytrain):
-    Numactions = Ytrain.shape[0]/Xtrain.shape[0]
-    d = Ytrai.shape[1]  
-    G = []
-    h = []
-    weights = zeros(num_actions*d, d);                                                                                                                                                        
-    for k=1:num_actions:
-        Xcat = [];                                                                                                                                                                               Ycat = [];                                                                                                                                                                               for i=1:size(Xtrain,1):                                                                                                                                                                        Xcat = cat(1,Xcat,Xtrain(i,:))                                                                                                                                                          Ycat = cat(1,Ycat,Ytrain((i-1)*num_actions + k,:))                                                                                                                                  weights((k-1)*d + 1 : k*d, :) = Ycat'*pinv(Xcat')                                                                                                                                        [u,s,v] = svds(weights((k-1)*d + 1 : k*d, :),1)                                                                                                                                      
-        fprintf('top singular value = %f\n',s);                                                                                                                                       
-        tmp = u*v';                                                                                                                                                                              ebar = tmp(:);                                                                                                                                                                           G = [G;ebar'];                                                                                                                                                                           h = [h;1];                                                                                                                                                                                                                                                                                                            
-    
+                                                                                                                                                                                                                                                                                                            
+def learn_stable_Op(Xtrain,Ytrain):
+	num_actions = size(Ytrain,1) / size(Xtrain,1)
+	d = size(Xtrain,2)
+	G = []
+	h = []
+	weights = zeros(num_actions*d, d)
+	for k in range(num_actions):
+    		Xcat = []
+    		Ycat = []
+    		for i in range(size(Xtrain,1)):
+        		Xcat = cat(1,Xcat,Xtrain(i,:))
+        		Ycat = cat(1,Ycat,Ytrain((i-1)*num_actions + k,:))
+
+    		weights((k-1)*d + 1 : k*d, :) = Ycat'*pinv(Xcat')
+    		[u,s,v] = svds(weights((k-1)*d + 1 : k*d, :),1)
+    		fprintf('top singular value = %f\n',s)                                                                                                                                        
+    		tmp = u*v'
+    		ebar = tmp(:)
+    		G = [G;ebar']
+    		h = [h;1]
+	
+	for i in range(15):
+    		cvx_begin quiet
+    		variable w(num_actions*d, d);
+    		variable b(num_actions,d);
+    		loss = L2Loss(Xtrain,Ytrain,w,b);
+    		minimize (loss)
+    		subject to
+    		for k in range(num_actions):
+		       wvec = w((k-1)*d + 1 : k*d, :) 
+			 -G*wvec(:) + h >= 0
+       		fprintf('Training loss = %f\n',loss);                                                                                                                                                 
+    		for k in range(num_actions):
+        		[u,s,v] = svds(w((k-1)*d + 1 : k*d, :),1)
+        		fprintf('top singular value for action %d = %f\n',k,s);                                                                                                                           
+        		tmp = u*v';
+        		ebar = tmp(:);
+        		G = [G ; ebar'];
+        		h = [h ; 1];
+    		d = size(Xtrain,2);                                                                                                                                                                       
+                                                                                                                                                                                          
+		G = [];                                                                                                                                           
+
+	return w,b,loss
