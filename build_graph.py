307fe4c5c43a9f87ad55f8fc4a49b5868f31d4b4
ankush1123roy@gmail.com <ankush1123roy@gmail.com>
2013-10-28 06:38:27 -0600

    Verified

--- build_graph.py
+++ build_graph.py
@@ -1,33 +1,38 @@
 """ Original Matlab Code by Kiana Hajebi 
 Changed to Python by Ankush Roy. This works for K = 1"""
 from knnsearch import knnsearch
+import random
 import numpy
 from numpy import matrix as MA
 def  build_graph(X, k):
     dt = numpy.dtype('f8')
     f=[]
-    nodes = single(numpy.zeros((X.shape[1],k),dt));
+    nodes = numpy.zeros((X.shape[1],k),dt);
     for i in range(1,X.shape[0]):
         print i
-        query = X[i:]
-        (nns_inds, nns_dists) = knnsearch(double[query], double[X], k+1)
-        f = find(nns_inds==i);
-
-        if not isempty(f):
-            nns[f:] = [];
-            nns_inds[f] = [];
-    
-        for j in range(1,length(nns_inds)):
-            nodes[i:] = nns_inds
-
+        query = MA(X[(i-1),0:],dt)
+        (nns_inds, nns_dists) = knnsearch(query,X,k+1)
+        I  = 0 
+        f = []
+        while I < len(nns_inds):
+            if nns_inds[I] == i-1:
+                nns_inds.remove(i-1)
+                nodes[i,0:] = nns_inds
+                break            
+            else:
+                I += 1
+    print nodes
     return nodes
 
 if __name__ == '__main__':
-    X = [i in range(10)]
+    dt = numpy.dtype('f8')
+    X = [range(10)]
+    print X
     for i in range(10):
         Vect = [random.randint(0,100) for r in range(10)]
         X = numpy.vstack([X,Vect])
     X = numpy.delete(X,(0),axis = 0)
+    X = MA(X,dt)
+    K = 2
     print X
-    K = 1
     build_graph(X,K)
--- knnsearch.py
+++ knnsearch.py
@@ -1,63 +1,21 @@
-import numpy 
-from copy import deepcopy
+import math
+import numpy
 from numpy import matrix as MA
-def knnsearch(varargin):
-    (Q,R,K,fident) = parseinputs(varargin{:})
-    error(nargoutchk(0,2,nargout))
-    N,M = Q.shape[0] Q.shape[1]
-    L = R.shape[0]
-    if M !=R.shape[1]:
-        print ('Index exceeds matrix Dimensions .. ')
-        return
-    idx = numpy.zeros((N,K),dt4)
-    D = idx
-    if K==1:
-        for k in range(1,N+1):
-            d=numpy.zeros((L,1),dt4)
-            for t in range(1,M+1):
-                d=d+(R(:,t)-Q(k,t)).^2 # This loop ends here
-            if fident:
-                d[k]=inf
-
-            [D(k),idx(k)]=min(d)
-    else:
-        for k in range(1,N+1):
-            d=numpy.zeros((L,1),dt4)
-            for t in range(1:M+1):
-                d=d+(R(:,t)-Q(k,t)).^2
-
-            if fident:
-                d(k)=inf
-            s = deepcopy(d)
-            t  = sorted(range(len(s)),key=lambda x:s[x])
-            s.sort()
-
-        idx[k,:]=t[1:K]
-        D[k,:]=s[1:K]
-
-    if nargout>1:
-        D=numpy.sqrt(D)
-
-def parseinputs(varargin):
-    error(nargchk(1,3,nargin));
-    Q=varargin{1};
-    if nargin<2:
-        R=Q
-        fident = true
-    else:
-        fident = false
-        R=varargin{2}
-    
-    if isempty(R):
-        fident = true
-        R=Q
+from copy import deepcopy
+def knnsearch(Q,X,k):
+    index1 = []
+    dist = []
+    for i in range(len(X)): 
+        distVec = (X[i,0:] - Q)*numpy.transpose(X[i,0:] - Q)
+        dist.append(math.sqrt(sum(distVec)))
+    Dist = deepcopy(dist)
+    dist.sort()
+    minDist = dist[0:k]
+    I = 0 
+    while I < k:
+        index1.append(Dist.index(dist[I]))
+        I += 1
+    return  index1, minDist
+                  
         
-    if not fident:
-        fident = isequal(Q,R)
 
-    if nargin < 3:
-        K=1
-    else:
-        K=varargin{3}
-
-return Q,R,K,fident
